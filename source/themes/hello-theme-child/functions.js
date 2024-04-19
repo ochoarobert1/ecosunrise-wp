e83691cf21e33e8b36dff6fb205b567a1362f737
Robert Ochoa <ochoa.robert1@gmail.com>
2024-04-19 11:39:06 -0400

    Add theme features - Styles

--- source/themes/hello-theme-child/functions.js
+++ source/themes/hello-theme-child/functions.js
@@ -1,6 +1,22 @@
-document.addEventListener( 'wpcf7mailsent', function ( event ) {
-    gtag( 'event', 'wpcf7_submission', {
-        'event_category': "form_submit",
-        'event_label': event.detail.unitTag
-    } );
-}, false );
+let landingHeader = document.getElementById("landingHeader");
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var scroll = window.scrollY;
+    if (scroll > 50) {
+      landingHeader.classList.add("landing-sticky");
+    } else {
+      landingHeader.classList.remove("landing-sticky");
+    }
+  });
+});
+
+document.addEventListener(
+  "wpcf7mailsent",
+  function (event) {
+    gtag("event", "wpcf7_submission", {
+      event_category: "form_submit",
+      event_label: event.detail.unitTag,
+    });
+  },
+  false
+);
--- source/themes/hello-theme-child/functions.php
+++ source/themes/hello-theme-child/functions.php
@@ -10,38 +10,41 @@
  * @package HelloElementorChild
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
+define('HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0');
 
 /**
  * Load child theme scripts & styles.
  *
  * @return void
  */
-function hello_elementor_child_scripts_styles() {
+function hello_elementor_child_scripts_styles()
+{
 
-	wp_enqueue_style(
-		'hello-elementor-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		[],
-		HELLO_ELEMENTOR_CHILD_VERSION
-	);
-	
-	wp_enqueue_script(
-		'hello-elementor-child-script',
-		get_stylesheet_directory_uri() . '/functions.js',
-		[
-			'jquery'
-		],
-		HELLO_ELEMENTOR_CHILD_VERSION,
-		true
-	);
+    wp_enqueue_style(
+        'hello-elementor-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        [],
+        HELLO_ELEMENTOR_CHILD_VERSION
+    );
+
+    wp_enqueue_script(
+        'hello-elementor-child-script',
+        get_stylesheet_directory_uri() . '/functions.js',
+        [
+            'jquery'
+        ],
+        HELLO_ELEMENTOR_CHILD_VERSION,
+        true
+    );
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+add_action('wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20);
 
 if (function_exists('add_image_size')) {
     add_image_size('gallery', 300, 300, array('center', 'center'));
 }
+
+require_once('inc/custom-taxonomy.php');
